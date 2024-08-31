7e231e997aabeb58f46ff5c5627f8d82241814f0
Edward... <45647882@comcel.com.co>
2024-08-31 02:35:04 -0500

    docs: :memo: Deploy
    Deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/tasksReact/'
+  base : 'https://germainTimana.github.io/tasksReact'
 })
